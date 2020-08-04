04f602379a06f0be31a880198846e0ff8d18e64a
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-08-03 21:00:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+/* 
 
+If ‘start’ is chosen in the dropdown message  :
+
+the background colour of the page should change to #C47C47
+ new div is added to the page. The div contains an H1 with the class of ‘welcome_heading” and text content that says ‘Welcome To Your Final Exam. Good Luck’
+If ‘complete’ is chosen from the dropdown message: 
+
+The background color of the page should change to #C0FFEEA new div is added to the page.
+ The div contains an H1 with the class of ‘finished heading’ and text content that says “Great Job! Thanks for a Great Semester”! 
+Remember to ensure your code is properly formatted and valid! Create a folder called Q1 and include all your code files inside. Publish your page using Github Pages. Include a link to your GitHub repo and published page as your answer.
+
+*/
